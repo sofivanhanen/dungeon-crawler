375539a5a07175dadf15f32422bfecb79070446f
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-06 21:55:08 +0300

    Add attacking with animation
    No damage done yet, and animation is poor quality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,14 @@ public class PlayerController : MonoBehaviour {
 	const float speedOfMovement = 5.0f;
 	const float speedOfTurn = 0.15f;
 
+	public GameObject sword;
+
 	void Start () {
-		
 	}
 	
 	void Update () {
+		
+		// Moving
 		var x = Input.GetAxis("Horizontal");
         var z = Input.GetAxis("Vertical");
 		if (!(x==0 && z==0)) {
@@ -19,6 +22,14 @@ public class PlayerController : MonoBehaviour {
 		// TODO: Why does rotation work wrong? Player always faces opposite direction. '*-1* fixes it.
 		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement*-1), speedOfTurn);
         transform.Translate(movement * Time.deltaTime * speedOfMovement, Space.World);
+		}
+		
+		// Shooting
+		if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("Mouse down.");
+			SwordController swordController = sword.GetComponent<SwordController>();
+			swordController.Attack();
 		}
 	}
 }
---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+
+	private Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	public void Attack() {
+		animator.SetTrigger("Sword_attack");
+	}
+	
+}
