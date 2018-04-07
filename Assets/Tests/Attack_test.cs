d3fdc47f93dd020df4b8f4c73239eb53b421dcdc
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-07 21:36:36 +0300

    Add test for attacking

---
+++ Assets/Tests/Attack_test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class Attack_test 
+{
+
+    [UnityTest]
+    public IEnumerator AttackStartsSwingAnimation()
+    {
+        // Not yet tested
+        var sword = new GameObject();
+        var controller = sword.AddComponent<SwordController>();
+        var animator = sword.AddComponent<Sword_anim_controller>();
+        sword.Attack();
+        yield return null; // Skip a frame
+        Assert.True(GetCurrentAnimatorStateInfo(0).IsName("Sword_swing"));
+    }
+}
