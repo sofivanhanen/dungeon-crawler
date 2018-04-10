36f4c31f0dd6d3f6b127c49748135f46fcc3f7ca
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-10 14:24:08 +0300

    Ghost moves towards player

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+
+	public GameObject player;
+	private const int maxRange = 5;
+	private const float speedOfMovement = 1.0f;
+	private const float speedOfTurn = 0.15f;
+
+	void Start ()
+	{
+		player = GameObject.FindWithTag("Player");
+	}
+	
+	void Update () {
+		var heading = player.transform.position - this.transform.position;
+		// Using sqrMagnitude to save processing power
+		if (heading.sqrMagnitude < maxRange * maxRange) {
+			// Player is within range! Let's get 'em!!
+			var distance = heading.magnitude;
+			var movement = heading / distance;
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement*-1), speedOfTurn);
+			transform.Translate(movement * Time.deltaTime * speedOfMovement, Space.World);
+		}
+	}
+}
