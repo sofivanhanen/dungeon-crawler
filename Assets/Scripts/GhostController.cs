79683f7bfaff8823a555f889b7cd113260cc92ae
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-10 16:31:00 +0300

    Player can hurt ghosts

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -17,15 +17,24 @@ public class GhostController : MonoBehaviour
 
 	private const int damage = 30;
 
+	private const int maxHealth = 40;
+	private int health;
+
+	private const float minTimeBetweenGettingHit = 0.3f;
+	private float hitTimer;
+
 	void Start ()
 	{
 		player = GameObject.FindWithTag("Player");
 		attackTimer = minTimeBetweenAttacks;
+		health = maxHealth;
+		hitTimer = minTimeBetweenAttacks;
 	}
 	
 	void Update ()
 	{
 		attackTimer += Time.deltaTime;
+		hitTimer += Time.deltaTime;
 		var heading = player.transform.position - this.transform.position;
 		// Using sqrMagnitude to save processing power
 		if (heading.sqrMagnitude < maxRange * maxRange) {
@@ -39,10 +48,18 @@ public class GhostController : MonoBehaviour
 
 	void OnCollisionStay(Collision collision)
 	{
-		if(collision.gameObject.name == "Player" && attackTimer >= minTimeBetweenAttacks)
+		if(collision.gameObject.tag == "Player" && attackTimer >= minTimeBetweenAttacks)
 		{
 			player.GetComponent<PlayerController>().GetHit(damage);
 			attackTimer = 0;
 		}
+	}
+
+	public void GetHit(int damage)
+	{
+		if (hitTimer < minTimeBetweenGettingHit) return;
+		health -= damage;
+		if (health <= 0) Destroy(this.gameObject);
+		hitTimer = 0f;
 	}
 }
--- Assets/Scripts/SwordController.cs
+++ Assets/Scripts/SwordController.cs
@@ -7,12 +7,24 @@ public class SwordController : MonoBehaviour
 
 	private Animator animator;
 
+	private const int damage = 20;
+
 	void Start () {
 		animator = GetComponent<Animator>();
 	}
 
 	public void Attack() {
 		animator.SetTrigger("Sword_attack");
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		// Possible because of multiple contact points, OnTriggerEnter gets called repeatedly. Handle this in GetHit.
+		if (collider.gameObject.tag == "Enemy" && animator.GetCurrentAnimatorStateInfo(0).IsName("Sword_swing"))
+		{
+			// TODO What if it's another enemy, not a ghost?
+			collider.gameObject.GetComponent<GhostController>().GetHit(damage);
+		}
 	}
 	
 }
