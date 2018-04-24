0979f17f6e99ae4cbf2eb42bb663e9a53f4138bb
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-24 14:33:12 +0300

    Add HealthAndDying behaviour controller and ObjectWithHealth interface

---
+++ Assets/Scripts/BehaviourControllers/HealthAndDyingBehaviourController.cs
@@ -0,0 +1,76 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace BehaviourControllers
+{
+    // Controls the health and dying of objects implementing IObjectWithHealth
+    public class HealthAndDyingBehaviourController
+    {
+        public readonly IObjectWithHealth ObjectWithHealth;
+
+        public Color NormalColor;
+        public Color HurtingColor;
+
+        public int MaxHealth;
+        public int Health;
+        public bool Dead;
+
+        public readonly float DamageTakingTimerMax;
+        private float _timeSinceLastHit;
+
+        public HealthAndDyingBehaviourController(IObjectWithHealth objectWithHealth, Color normalColor,
+            Color hurtingColor, int maxHealth, float damageTakingTimerMax)
+        {
+            ObjectWithHealth = objectWithHealth;
+            NormalColor = normalColor;
+            HurtingColor = hurtingColor;
+            MaxHealth = maxHealth;
+            Health = MaxHealth; // Assuming we start at full health
+            Dead = false; // Assuming we start alive
+            DamageTakingTimerMax = damageTakingTimerMax;
+            _timeSinceLastHit = damageTakingTimerMax;
+        }
+
+        // Returns true if damage was taken, false otherwise
+        public bool GetHit(int damage, bool useTimer)
+        {
+            if (Dead) return false;
+
+            // TODO: This timer might end up being redundant
+            if (useTimer)
+            {
+                if (_timeSinceLastHit <= DamageTakingTimerMax)
+                {
+                    return false;
+                }
+            }
+
+            Health -= damage;
+            if (Health <= 0)
+            {
+                Dead = true;
+                ObjectWithHealth.Die();
+                return true;
+            }
+
+            ObjectWithHealth.ChangeColor(HurtingColor);
+            _timeSinceLastHit = 0f;
+            return true;
+        }
+
+        // The 'master' object calls this so this controller can be in charge of color changing and other time-sensitive things
+        public void Update(float deltaTime)
+        {
+            if (Dead) return;
+            _timeSinceLastHit += deltaTime;
+            if (_timeSinceLastHit > DamageTakingTimerMax && _timeSinceLastHit - deltaTime < DamageTakingTimerMax)
+            {
+                // Changing from hurting to normal
+                ObjectWithHealth.ChangeColor(NormalColor);
+            }
+        }
+
+        // TODO: Regenerating health
+        // TODO: Increasing max health
+    }
+}
---
+++ Assets/Scripts/Interfaces/IObjectWithHealth.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Interfaces
+{
+    // Player and Enemy implement this, as they have health and can be killed
+    public interface IObjectWithHealth
+    {
+        
+        // When getting hit
+        void ChangeColor(Color newColor);
+        
+        // When killed
+        void Die();
+    }
+}
