daf9e749df3ba76ed6e8c84b22d85e1104159de8
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-29 14:12:55 +0300

    Refactor SwordController

--- Assets/Scripts/GameObjectControllers/SwordController.cs
+++ Assets/Scripts/GameObjectControllers/SwordController.cs
@@ -1,26 +1,29 @@
 ﻿using UnityEngine;
-using GameObjectControllers;
 
-public class SwordController : MonoBehaviour
+namespace GameObjectControllers
 {
-    private const int Damage = 20;
-
-    private Animator _animator;
-
-    private void Start()
+    public class SwordController : MonoBehaviour
     {
-        _animator = GetComponent<Animator>();
-    }
+        private const int Damage = 20;
 
-    public void Attack()
-    {
-        _animator.SetTrigger("Sword_attack");
-    }
+        private Animator _animator;
 
-    private void OnTriggerEnter(Collider colliderOfSecondObject)
-    {
-        // Possible because of multiple contact points, OnTriggerEnter gets called repeatedly. Handle this in GetHit.
-        if (colliderOfSecondObject.gameObject.CompareTag("Enemy") && _animator.GetCurrentAnimatorStateInfo(0).IsName("Sword_swing"))
-            colliderOfSecondObject.gameObject.GetComponent<GhostController>().GetHit(Damage);
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void Attack()
+        {
+            _animator.SetTrigger("Sword_attack");
+        }
+
+        private void OnTriggerEnter(Collider colliderOfSecondObject)
+        {
+            // Possible because of multiple contact points, OnTriggerEnter gets called repeatedly. Handle this in GetHit.
+            if (colliderOfSecondObject.gameObject.CompareTag("Enemy") &&
+                _animator.GetCurrentAnimatorStateInfo(0).IsName("Sword_swing"))
+                colliderOfSecondObject.gameObject.GetComponent<GhostController>().GetHit(Damage);
+        }
     }
 }
