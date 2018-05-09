3dc884668fc0bcf68fa2275efaac7ecae04ba419
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-05-09 10:57:29 +0300

    XML tags / documentation for Rooms

--- Assets/Scripts/LevelGeneration/Rooms/Room.cs
+++ Assets/Scripts/LevelGeneration/Rooms/Room.cs
@@ -2,20 +2,27 @@
 
 namespace LevelGeneration.Rooms
 {
+    /// <summary>
+    /// Base class for Room objects
+    /// </summary>
     public class Room
     {
+        // All rooms need to know their location, orientation and shape.
+        // Rooms also keep track of the enemies, whether they're the beginning or end of a level,
+        // and whether their southern or western wall clips with another room.
+        
+        // X and Z as their index in a two-dimensional array, not as Unity distance values.
         public readonly int X;
         public readonly int Z;
+        // Orientation is saved as int, 0-3, corresponding to directions defined in LevelGenerator as constants.
+        // 0 means default (south), 1 means to west (turned 90 degrees), and so on.
         public readonly int Orientation;
         public int Enemies;
-
         public bool Beginning;
         public bool End;
         public bool HideSouthernWall;
         public bool HideWesternWall;
 
-        // All rooms need to know their location, orientation and shape.
-        // Rooms also keep track of the enemies, loot, and specialities (i.e. staircases).
         protected Room(int x, int z, int orientation)
         {
             X = x;
@@ -28,6 +35,12 @@ namespace LevelGeneration.Rooms
             HideWesternWall = false;
         }
 
+        /// <summary>
+        /// Checks if the room has a doorway to the specified direction. Implemented in children.
+        /// </summary>
+        /// <param name="direction">Direction to check. 0 is south, 1 is west, and so on.</param>
+        /// <returns>True if room has a doorway to the direction, false otherwise.</returns>
+        /// <exception cref="Exception">If trying to call from Room. Should call from children.</exception>
         public virtual bool HasDoorwayTo(int direction)
         {
             // Implement in children
